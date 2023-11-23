4a609075b1004d487bc9d278217971d0f5d68e3b
rammroumm2K <ramybrz00@gmail.com>
2023-11-23 15:15:29 +0100

    puff2

--- stagiaires/Rami/01-hello-world.php
+++ stagiaires/Rami/01-hello-world.php
@@ -1,3 +1,6 @@
 <?php
 echo "Hello World";
-?>
+$MyString = // string en Pascalcase
+$bool = true; // ou false : variable binaire
+$is_null = NULL; // Variable ne contenant... rien (NULL) !
+// ne confondre avec vide (Empty)
