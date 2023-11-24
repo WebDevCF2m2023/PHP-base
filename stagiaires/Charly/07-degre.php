bbe5d1a426e74244bc0ccb002a6a13934c9058f1
Charly0475 <charly.ongemba.ngongo@cf2000.onmicrosoft.com>
2023-11-24 10:18:51 +0100

    correction

--- stagiaires/Charly/07-degre.php
+++ stagiaires/Charly/07-degre.php
@@ -1,10 +1,10 @@
 <?php 
-$temp=100
-$if (temp >= mt_rand) 
-$echo{"L’eau s'évapore!"} 
-$temp {temp=< 99}
-$echo{"l'eau boue"}
-$ else 
-$variable temp<0
-$echo{"l'eau est solide"}
+$temp=mt_rand(-100,200);
+$if (temp >= mt_rand);
+$echo{"L’eau s'évapore!"} /n;
+$temp {temp=< 99};
+$echo{"l'eau boue"}/n;
+$ else;
+$variable temp<0/n;
+$echo{"l'eau est solide"};
 ?>
