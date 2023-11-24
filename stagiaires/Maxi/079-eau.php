657de87b6ba4273f80c4de7ef78d0b1435f4c303
armaxro <arnaldomrojas@outlook.com>
2023-11-24 09:08:35 +0100

    correction && ||

--- stagiaires/Maxi/079-eau.php
+++ stagiaires/Maxi/079-eau.php
@@ -3,7 +3,7 @@
 $eau = mt_rand(-100,200);
 if($eau <=0){
     echo "Solide à $eau °C";
-}elseif($eau <=1 && $eau <=99){
+}elseif($eau <=1 || $eau <=99){
     echo "Liquide à $eau °C";
 }else{
     echo "Gazeuse à $eau °C";
