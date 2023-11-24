c88f7b70943229fa922e8b0dbe2c82ac0f9b49ef
leerlandais <leerlandais@gmail.com>
2023-11-24 12:01:29 +0100

    Ex 10 et 11

--- stagiaires/Lee/09-eau.php
+++ stagiaires/Lee/09-eau.php
@@ -7,7 +7,7 @@ $temp = mt_rand(-100,200);
 if($temp <= 0){
     echo "L'eau est solide à $temp °C";
 }
-elseif($temp >= 1 && $temp <= 99){
+elseif($temp <= 99){
     echo "L'eau est liquide à $temp °C";
 }
 elseif($temp > 100){
