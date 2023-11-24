201c48f2751f8c1b5f3fc95a7168315e333562b4
simonaiuliana <Iuliana.hoater@cf2000.onmicrosoft.com>
2023-11-24 12:07:03 +0100

    modification ex 11

--- stagiaires/Simona/ex11.php
+++ stagiaires/Simona/ex11.php
@@ -4,13 +4,13 @@ $chiffre = rand(0, 10);
 
 
 if ($chiffre >= 0 && $chiffre <= 3) {
-    $resultat = "EXACTEMENT : {$chiffre} : Nul, étudie la prochaine fois";
+    echo"EXACTEMENT : {$chiffre} : Nul, étudie la prochaine fois";
 } elseif ($chiffre >= 4 && $chiffre <= 5) {
-    $resultat = "{$chiffre} : Peut mieux faire";
+   echo"{$chiffre} : Peut mieux faire";
 } elseif ($chiffre >= 6 && $chiffre <= 7) {
-    $resultat = "{$chiffre} : Bien";
+    echo"{$chiffre} : Bien";
 } else {
-    $resultat = "{$chiffre} : Très bien";
+    echo"{$chiffre} : Très bien";
 }
-echo $resultat;
+
 ?>
