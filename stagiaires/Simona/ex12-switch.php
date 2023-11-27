0eacc0482f17078a86070e09b6a039507d853613
simonaiuliana <Iuliana.hoater@cf2000.onmicrosoft.com>
2023-11-27 12:12:49 +0100

    modification

--- stagiaires/Simona/ex12-switch.php
+++ stagiaires/Simona/ex12-switch.php
@@ -1,22 +1,22 @@
 <?php
 $cifre = mt_rand(0,10);
 switch($cifre){
+    case 0:
     case 1:
     case 2:
     case 3:
-    case 4:
         echo"{$cifre} : Nul, étudie la prochaine fois";
         break;
+    case 4:
     case 5:
-    case 6:
         echo"{$cifre} : Peut mieux faire";
         break;
+    case 6:
     case 7:
-    case 8:
         echo"{$cifre} : Bien";
         break;
     default:
-       echo "Très bien";
+       echo "{$cifre} : Très bien";
     
 
 
