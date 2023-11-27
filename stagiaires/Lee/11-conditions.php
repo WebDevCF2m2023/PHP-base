e11288e13c8c20b7b4476033731eaf78edfcbb3a
leerlandais <leerlandais@gmail.com>
2023-11-27 11:40:51 +0100

    Ex12

--- stagiaires/Lee/11-conditions.php
+++ stagiaires/Lee/11-conditions.php
@@ -5,10 +5,10 @@ $myNum = mt_rand(0, 10);
 if($myNum < 4){
     echo "$myNum : Nul, étudie la prochaine fois";
 }
-else if($myNum < 6){
+elseif($myNum < 6){
     echo "$myNum : Peut faire mieux";
 }
-else if($myNum <8){
+elseif($myNum <8){
     echo "$myNum : Bien";
 }
 else{
