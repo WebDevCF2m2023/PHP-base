dbb6af35750116a2fde6b1e6d40b3b18730902cc
Charly0475 <charly.ongemba.ngongo@cf2000.onmicrosoft.com>
2023-11-27 09:56:22 +0100

    retour

--- stagiaires/Charly/07-degre.php
+++ stagiaires/Charly/07-degre.php
@@ -4,5 +4,4 @@ echo$temp."<br>";
 if ($temp >= 100){echo"L’eau s'évapore!\n";
 }
 elseif($temp<0){echo"l'eau est solide\n";}
-else if($temp<= 99){echo"l'eau boue\n";}
-?>
+else ($temp<= 99){echo"l'eau boue\n";}
---
+++ stagiaires/Charly/10-info-sever.php
@@ -0,0 +1,1 @@
+<? echo{httpd,apache(2)}
