d5a052dbce79420351523583ac6176132e481900
Guillaume <guillaumebaillieux88@gmail.com>
2023-11-27 11:42:43 +0100

    ex12

---
+++ stagiaires/Guillaume/12b-switch.php
@@ -0,0 +1,21 @@
+<?php
+
+$a(mt_rand(0,10));{
+
+case "1"($a<=3):
+  echo "$a - Nul, étudie la prochaine fois";
+  break;
+
+case "2"($a <=5):
+  echo "$a - Peut mieux faire";
+  break;
+
+case "3"($a <=7):
+    echo "$a - Bien";
+    break;
+
+default:
+  echo "$a - Très bien";
+
+}
+
