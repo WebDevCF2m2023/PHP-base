756a822fb6044bb75ba423aa28501a0662657e91
armaxro <arnaldomrojas@outlook.com>
2023-12-01 15:10:47 +0100

    accueil

--- stagiaires/Maxi/14-Include/inc/header.php
+++ stagiaires/Maxi/14-Include/inc/header.php
@@ -3,4 +3,4 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
+    <title>Include</title>
--- stagiaires/Maxi/14-Include/index.php
+++ stagiaires/Maxi/14-Include/index.php
@@ -1,10 +1,14 @@
-<?php
-include "inc/header.php";
-
-?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
 <body>
-    
+    <h1>Include</h1>
+    <nav>
+        <a href="./">Accueil Include</a> - <a href="./?section=bio">Biograpghie</a>
+    </nav>
 </body>
 </html>
-
