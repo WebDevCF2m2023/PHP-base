52f9e975f06044f8b4e5bab516c35957981fe6b0
mikhawa <michaeljpitz@gmail.com>
2023-12-06 16:15:12 +0100

    pages

--- 18-front-controller/templates/actualites.php
+++ 18-front-controller/templates/actualites.php
@@ -1,1 +1,35 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <title>Actualités de 18-front-controller</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+    <h1>18-front-controller</h1>
+    </header>
+        <?php include 'inc/menu.php'; ?>
+    <section>
+        <h2>Actualités</h2>
+        <h3>1. Nouvelle Version Majeure de FrontController 3.0</h3>
+        <p>La communauté PHP accueille avec enthousiasme la sortie de la version 3.0 de FrontController, le célèbre framework de contrôleurs frontal. Cette mise à jour apporte des fonctionnalités révolutionnaires, une meilleure gestion des dépendances, et des améliorations significatives des performances. Les développeurs sont invités à mettre à jour leurs projets pour profiter des dernières avancées dans la gestion frontale en PHP.</p>
+
+        <h3>2. Webinar Exclusif : Les Secrets des Contrôleurs Frontaux Révélés</h3>
+        <p>Rejoignez-nous le mois prochain pour un webinar exclusif où des experts éminents partageront leurs connaissances sur les stratégies avancées de contrôleurs frontal en PHP. Découvrez les astuces pour optimiser vos applications, résoudre des problèmes courants, et explorer les nouvelles tendances qui redéfiniront le paysage du développement web.</p>
+
+        <h3>3. Astuces de Performance : Accélérez vos Contrôleurs Frontaux</h3>
+        <p>Dans notre dernier article, plongez dans les astuces de performance essentielles pour maximiser l'efficacité de vos contrôleurs frontaux en PHP. Des conseils pratiques sur la gestion du cache, l'optimisation des requêtes, et l'utilisation judicieuse des bibliothèques externes vous permettront d'offrir des expériences utilisateur plus rapides et plus fluides.</p>
+
+        <h4>4. Meetup Communautaire : Échangez sur les Dernières Innovations Frontales</h4>
+        <p>Rejoignez notre prochain meetup communautaire dédié aux contrôleurs frontaux en PHP. Rencontrez des développeurs passionnés, échangez des idées novatrices, et participez à des discussions interactives sur les défis actuels et les solutions émergentes. Ensemble, continuons à faire progresser la gestion frontale dans l'écosystème PHP.</p>
+    </section>
+    <div id="nuage"></div>
+<?php include 'inc/footer.php'; ?>
+<script  src="js/script.js"></script>
+</body>
+</html>
--- 18-front-controller/templates/mentions-legales.php
+++ 18-front-controller/templates/mentions-legales.php
@@ -1,1 +1,45 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <title>Mentions légales de 18-front-controller</title>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <header>
+    <h1>18-front-controller</h1>
+    </header>
+        <?php include 'inc/menu.php'; ?>
+    <section>
+        <h2>Mentions légales </h2>
+        <h3>Propriété intellectuelle :</h3>
+
+        <p>Ce site et son contenu sont la propriété exclusive de CF2m. Toute reproduction, même partielle, est strictement interdite sans autorisation préalable.</p>
+
+        <h3>Collecte de données personnelles :</h3>
+
+        <p>Les données personnelles collectées sur ce site sont utilisées uniquement dans le cadre prévu lors de la collecte (ex. : formulaire de contact) et ne seront en aucun cas transmises à des tiers. Conformément à la loi, vous disposez d'un droit d'accès, de modification et de suppression de vos données personnelles.</p>
+
+        <h3>Cookies :</h3>
+
+        <p>Ce site utilise des cookies. En naviguant sur ce site, vous acceptez l'utilisation de cookies conformément à notre politique en matière de cookies.</p>
+
+        <h3>Responsabilité :</h3>
+
+        <p>Malgré tous les efforts déployés pour assurer l'exactitude des informations, nous déclinons toute responsabilité en cas d'erreurs ou d'omissions. Les liens vers des sites tiers ne sont fournis qu'à titre informatif, et nous n'assumons aucune responsabilité quant au contenu de ces sites.</p>
+
+        <h3>Droits d'auteur :</h3>
+
+        <p>Le contenu de ce site est protégé par les lois sur le droit d'auteur et les droits connexes. Tous les droits sont réservés.</p>
+
+
+    </section>
+    <div id="nuage"></div>
+<?php include 'inc/footer.php'; ?>
+<script  src="js/script.js"></script>
+</body>
+</html>
