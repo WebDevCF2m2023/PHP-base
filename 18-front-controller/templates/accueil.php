42e940341ea03c9fa15af17d6966d6baa665db22
WebDevCF2m <gitweb@cf2m.be>
2023-12-07 14:31:47 +0100

    18 + var

--- 18-front-controller/templates/accueil.php
+++ 18-front-controller/templates/accueil.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
-    <title>Accueil de 18-front-controller</title>
+    <title><?=$title?></title>
     <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
@@ -15,7 +15,7 @@
     </header>
         <?php include 'inc/menu.php'; ?>
     <section>
-        <h2>Accueil</h2>
+        <h2><?=$title?></h2>
         <p>Bienvenue sur <span class="txtBold">18-front-controller</span>.<br><br>Cet exemple montre une architecture séparant : <ul><li>Le <span class="txtBold">contrôleur frontal</span> <span class="txtItalic">public/index.php</span> ainsi que les autres fichiers disponibles au visiteur du site (dossier <span class="txtItalic">public</span>)</li><li>De la partie template (dossier <span class="txtItalic">templates</span>), qui fait donc partie backend du site car non visible pour le visiteur.</li></ul></p>
         <p>Seul le dossier <span class="txtItalic">public</span> et les fichiers/dossiers contenus ne devraient être accessibles à l'utilisateur du site.</p>
         <p>Cette séparation sera la première étape (et la seule demandée pour le <span class="txtBold">TI1 de Pierre et moi</span>) vers un modèle <span class="txtBold">MVC</span> (Model-View-Controller | <a href="https://developer.mozilla.org/fr/docs/Glossary/MVC" target="_blank">Lien</a>) :<br><br>
