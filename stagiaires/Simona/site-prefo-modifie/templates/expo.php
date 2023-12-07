c93733bf9afb0d0e125ea58f51843126fba5e919
simonaiuliana <Iuliana.hoater@cf2000.onmicrosoft.com>
2023-12-07 15:47:21 +0100

    site-prefo-modifie

---
+++ stagiaires/Simona/site-prefo-modifie/templates/expo.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>expo</title>
+        <link rel="stylesheet" href="expo.css">
+        
+        <head>  
+          
+    </head>
+    <body>
+        
+    <div class="container">
+        <ul>
+            <li><a href="?page=accueil">accueil</a></li>
+            <li><a href="?page=interview1">interview</a></li>
+            <li><a href="?page=expo">expo</a></li>
+            <li><a href="?page=conclusion">conclusion</a></li>
+            <li><a href="?page=galerie">galerie</a></li>
+         
+          </ul> 
+    
+        <h1>Regarder le passé pour comprendre le présent et anticiper l'avenir</h1>
+        <br>
+        <div id="expo">
+            
+       <p>Un regard dans le passé... dans le passé de l'ordinateur, mais aussi la découverte d'une échelle de mesure de l'évolution humaine.</p>
+       <p>À ce point, dans toute activité humaine est observée la présence de l'ordinateur. Qu'il s'agisse d'un travail, de loisirs ou simplement d'une recherche d'informations, une chose immatérielle, impliquait tellement de changements matériels.</p>
+       <p>Cette petite machine, autrefois de la taille d'une chambre, peut désormais nous transporter à travers le monde, nous mettre en contact avec n'importe qui et répondre à presque toutes les questions.</p>
+       <p>En regardant l'évolution de l'ordinateur, les films SF de l'enfance ne semblent plus un chose impossible. Je me souviens à quel point les films SF me semblaient incroyables et intouchables.</p> 
+       <p>Maintenant, quand je vois que je peux être servie au restaurant par un robot, je me demande quand viendra la jour où nous irons sur la lune en attendant la fusée comme le bus.</p> 
+       <p>À quoi s'attendre ? Nous devrions avoir peur ou participer à cette tempête de changements ? Chaque changement produit des effets, qu'ils soient positifs ou négatifs, il est certain que les choses ne seront plus comme avant.</p>
+       <p>Les nouvelles technologies telles que le calcul quantique et l'intelligence artificielle entraîneront le boom de l'industrie informatique.</p>
+       <p>Je regarde l'avenir avec peur, mais en même temps avec curiosité.</p> 
+       <p>L'évolution ne peut pas être contrôlée, tant que son développement dépend de nombreux facteurs.</p>
+       <p>A toi, lecteur, je te propose de faire un petit exercice: fais un pas en arrière et regarde l'évolution comme si tu la découvres pour la première fois. Regarde-la comme un enfant qui voit des flocons de neige pour la première fois et dis-moi ce que tu ressens.</p> 
+       <p>J'ai découverte une multitude de sentiments, mais j'ai surtout peur du fait que je n'ai pas trouvé la sécurité. Chose qui me fait penser au fait que l'humanité, dans son évolution technologique et informatique, connaîtra son autodestruction. Le besoin de sécurité est à la base de son évolution et son absence aura une influence négative.</p>
+       <p>Si nous voulons profiter des bienfaits de l'évolution, nous devons en supporter les conséquences. Il est impossible de contrôler les effets.</p> 
+       <p>Même si nous ne sommes que des observateurs de ce spectacle des changements, nous sommes tous touchés par ses effets.</p> 
+       <p>La frontière entre évolution et destruction est mince et tout dépend de la manière dont l'information est utilisée.</p>
+       <p>L'information est la chose la plus précieuse, un véritable trésor ou une véritable arme.</p> 
+       <p>L'histoire nous montre des nombreux exemples d'évolution et d'autodestruction.</p> 
+       <p>Idéalement, nous devrions tirer les leçons des erreurs du passé. Même si la société semble construite selon une matrice bien définie, la réalité est imprévisible.</p> 
+       <p>C'est donc à nous de choisir comment nous participerons aux changements de l'humanité.</p>
+       <p>Avec la peur dans le cœur, mais aussi avec l'espoir, je regarde vers l'avenir et j'atends de voir l'incroyable spectacle de l'évolution.</p>
+       
+    </div>
+     </div>   
+        
+        
+        
+                   
+                
+</body>
+</html>
---
+++ stagiaires/Simona/site-prefo-modifie/templates/galerie.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="fr">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>conclusion</title>
+        <link rel="stylesheet" href="galerie.css">
+        
+        <head>  
+            
+    
+
+<style>
+div.galerie {
+  margin: 5px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 180px;
+}
+
+div.galerie:hover {
+  border: 1px solid #777;
+}
+
+div.galerie img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+#galerie{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
+  margin: auto
+}
+
+#galerie section{
+  text-align: center;
+}
+
+#galerie section:nth-child(1) img{
+  width: 25%;
+}
+#galerie section:nth-child(2) img{
+  width: 35%;
+
+}
+
+
+</style>
+</head>
+<body>
+  
+
+
+  <main>
+    <ul>
+      <li><a href="index.html">accueil</a></li>
+      <li><a href="?page=interview1">interview</a></li>
+      <li><a href="?page=expo">expo</a></li>
+      <li><a href="?page=conclusion">conclusion</a></li>
+      <li><a href="?page=galerie">galerie</a></li>
+   
+    </ul> 
+
+
+    <article id="galerie">
+
+
+      <section>
+          <a target="_blank" href="galerie/p1.jpeg">
+            <img src="galerie/p1.jpeg" alt="p1" width="600" height="400" title="img1">
+          </a>
+          <a target="_blank" href="galerie/p3.jpeg">
+            <img src="galerie/p3.jpeg" alt="p3" width="600" height="400">
+          </a>
+          <a target="_blank" href="galerie/p5.jpeg">
+             <img src="galerie/p5.jpeg" alt="p5" width="600" height="400">
+          </a>
+          <a target="_blank" href="galerie/p6.jpeg">
+            <img src="galerie/p6.jpeg" alt="p6" width="600" height="400">
+          </a>
+          <a target="_blank" href="galerie/p9.jpeg">
+            <img src="galerie/p9.jpeg" alt="p9" width="600" height="400">
+          </a>
+      </section>
+
+      <section>
+          <a target="_blank" href="galerie/p2.jpg">
+            <img src="galerie/p2.jpg" alt="p2" width="600" height="400">
+          </a>
+          <a target="_blank" href="galerie/p4.jpg">
+            <img src="galerie/p4.jpg" alt="p4" width="600" height="400">
+          </a>
+          <a target="_blank" href="galerie/p7.jpg">
+            <img src="galerie/p7.jpg" alt="p7" width="600" height="400">
+          </a>
+          <a target="_blank" href="galerie/p8.jpg">
+            <img src="galerie/p8.jpg" alt="p8" width="600" height="400">
+          </a>
+      </section>
+
+
+    </article>
+
+  </main>
+
+     
+
+
+     
+          
+
+
+
+    </body>
+    </html>
