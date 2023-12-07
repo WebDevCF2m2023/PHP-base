86f28f7914166c7c0c45befa333720900427d92a
sebastien2506 <jestrovic_8@msn.com>
2023-12-07 15:26:50 +0100

    18

---
+++ stagiaires/Sebastien/my-front-controller.php/template/expo/galerie.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <link rel="stylesheet" href="css/style4.css" />
+    <link rel="icon" href="image video/logo.png" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=0.75" />
+    <title>photo galerie</title>
+  </head>
+  <body>
+    <nav>
+      <a href="?page=index">Accueil</a>
+      <a href="?page=interview">Interview</a>
+      <a href="?page=conclusion">Conclusion</a>
+      <a href="?page=expo">Expo</a>
+      <a href="?page=galerie">Photos</a>
+    </nav>
+    <div id="video-container">
+      <video src="image video/ben.mp4" autoplay loop muted></video>
+    </div>
+    <div id="image-track" data-mouse-down-at="0" data-prev-percentage="0">
+      <img class="image" src="image video/nam11.jpg" draggable="false" />
+      <img class="image" src="image video/nam2.jpg" draggable="false" />
+      <img class="image" src="image video/nam3.jpg" draggable="false" />
+      <img class="image" src="image video/nam4.jpg" draggable="false" />
+      <img class="image" src="image video/nam5.jpg" draggable="false" />
+      <img class="image" src="image video/nam6.jpg" draggable="false" />
+      <img class="image" src="image video/nam7.jpg" draggable="false" />
+      <img class="image" src="image video/nam9.jpg" draggable="false" />
+      <img class="image" src="image video/nam10.jpg" draggable="false" />
+      <img class="image" src="image video/nam12.jpg" draggable="false" />
+      <img class="image" src="image video/nam13.jpg" draggable="false" />
+      <img class="image" src="image video/nam14.jpg" draggable="false" />
+      <img class="image" src="image video/nam15.jpg" draggable="false" />
+      <img class="image" src="image video/nam17.jpg" draggable="false" />
+      <img class="image" src="image video/nam20.jpg" draggable="false" />
+    </div>
+    <script type="text/javascript" src="js/java.js"></script>
+  </body>
+</html>
---
+++ stagiaires/Sebastien/my-front-controller.php/template/interview/interview.php
@@ -0,0 +1,443 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <link rel="stylesheet" href="css/style2.css" />
+    <link rel="icon" href="image video/logo.png" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=0.75" />
+    <title>interview</title>
+  </head>
+  <body id="interview">
+    <nav>
+      <a href="?page=accueil">Accueil</a>
+      <a href="?page=interview">Interview</a>
+      <a href="?page=conclusion">Conclusion</a>
+      <a href="?page=expo">Expo</a>
+      <a href="?page=galerie">Photos</a>
+    </nav>
+
+    <div class="container">
+      <div class="leftside">
+        <div class="header">
+          <div class="userimg">
+            <img src="img interview/seb.png" class="cover" />
+          </div>
+          <ul class="nav_icons">
+            <li><ion-icon name="scan-circle-outline"></ion-icon></li>
+            <li><ion-icon name="chatbox"></ion-icon></li>
+            <li><ion-icon name="ellipsis-vertical"></ion-icon></li>
+          </ul>
+        </div>
+        <!--search-->
+        <div class="search_chat">
+          <div>
+            <input
+              type="text"
+              placeholder="rechercher ou démarrer une discussion"
+            />
+            <ion-icon name="search-outline"></ion-icon>
+          </div>
+        </div>
+        <!--chatlist-->
+        <div class="chatlist">
+          <div class="block active">
+            <div class="imgbx">
+              <img src="img interview/arnaud.jpg" class="cover" />
+            </div>
+            <div class="details">
+              <div class="listhead">
+                <h4>Arnaud Dumoncel</h4>
+                <p class="time">9:00</p>
+              </div>
+              <div class="message_p">
+                <p>courage pour t'as preformation ...</p>
+              </div>
+            </div>
+          </div>
+          <div class="block unread">
+            <div class="imgbx">
+              <img src="img interview/404.jpg" class="cover" />
+            </div>
+            <div class="details">
+              <div class="listhead">
+                <h4>Error404</h4>
+                <p class="time">08:00</p>
+              </div>
+              <div class="message_p">
+                <p>chaud patat🥔 aujourd'hui ont a la prefo ...</p>
+                <b>1</b>
+              </div>
+            </div>
+          </div>
+          <div class="block">
+            <div class="imgbx">
+              <img src="img interview/kev.jpg" class="cover" />
+            </div>
+            <div class="details">
+              <div class="listhead">
+                <h4>kevin</h4>
+                <p class="time">Hier</p>
+              </div>
+              <div class="message_p">
+                <p>Jesus loves you ...</p>
+              </div>
+            </div>
+          </div>
+          <div class="block unread">
+            <div class="imgbx">
+              <img src="img interview/1.jpg" class="cover" />
+            </div>
+            <div class="details">
+              <div class="listhead">
+                <h4>Michael</h4>
+                <p class="time">Hier</p>
+              </div>
+              <div class="message_p">
+                <p>Apres le git commit --m tu git push ORIGIN main ...</p>
+                <b>3</b>
+              </div>
+            </div>
+          </div>
+          <div class="block">
+            <div class="imgbx">
+              <img src="img interview/charly.jpg" class="cover" />
+            </div>
+            <div class="details">
+              <div class="listhead">
+                <h4>Charly</h4>
+                <p class="time">10/11/23</p>
+              </div>
+              <div class="message_p">
+                <p>t'as reussi ton problem de display ....</p>
+              </div>
+            </div>
+          </div>
+          <div class="block">
+            <div class="imgbx">
+              <img src="img interview/emr.png" class="cover" />
+            </div>
+            <div class="details">
+              <div class="listhead">
+                <h4>Emrah</h4>
+                <p class="time">09/11/23</p>
+              </div>
+              <div class="message_p">
+                <p>C koi un site ?😵</p>
+              </div>
+            </div>
+          </div>
+          <div class="block">
+            <div class="imgbx">
+              <img src="img interview/enez.jpg" class="cover" />
+            </div>
+            <div class="details">
+              <div class="listhead">
+                <h4>Enez</h4>
+                <p class="time">07/11/23</p>
+              </div>
+              <div class="message_p">
+                <p>t'as fait ton fut champion ⚽</p>
+              </div>
+            </div>
+          </div>
+          <div class="block">
+            <div class="imgbx">
+              <img src="img interview/era.png" class="cover" />
+            </div>
+            <div class="details">
+              <div class="listhead">
+                <h4>Eran</h4>
+                <p class="time">20/10/23</p>
+              </div>
+              <div class="message_p">
+                <p>cva tas bien dormis</p>
+              </div>
+            </div>
+          </div>
+          <div class="block">
+            <div class="imgbx">
+              <img src="img interview/lau.png" class="cover" />
+            </div>
+            <div class="details">
+              <div class="listhead">
+                <h4>Laura</h4>
+                <p class="time">20/10/23</p>
+              </div>
+              <div class="message_p">
+                <p>👀</p>
+              </div>
+            </div>
+          </div>
+          <div class="block">
+            <div class="imgbx">
+              <img src="img interview/irl.jpg" class="cover" />
+            </div>
+            <div class="details">
+              <div class="listhead">
+                <h4>Lee</h4>
+                <p class="time">19/10/23</p>
+              </div>
+              <div class="message_p">
+                <p>Pour connaitre le jour de ta naissance 📆...</p>
+              </div>
+            </div>
+          </div>
+          <div class="block">
+            <div class="imgbx">
+              <img src="img interview/anth.png" class="cover" />
+            </div>
+            <div class="details">
+              <div class="listhead">
+                <h4>Anthony</h4>
+                <p class="time">18/10/23</p>
+              </div>
+              <div class="message_p">
+                <p>Seb, as-tu un parapluie ☂ à me prêter ?</p>
+              </div>
+            </div>
+          </div>
+          <div class="block">
+            <div class="imgbx">
+              <img src="img interview/baris.jpg" class="cover" />
+            </div>
+            <div class="details">
+              <div class="listhead">
+                <h4>Baris</h4>
+                <p class="time">16/10/23</p>
+              </div>
+              <div class="message_p">
+                <p>Avec ou sans sauce?</p>
+              </div>
+            </div>
+          </div>
+          <div class="block">
+            <div class="imgbx">
+              <img src="img interview/gui.jpg" class="cover" />
+            </div>
+            <div class="details">
+              <div class="listhead">
+                <h4>Guillaume</h4>
+                <p class="time">15/10/23</p>
+              </div>
+              <div class="message_p">
+                <p>viking hahaaaaaa</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="rightside">
+        <div class="header">
+          <div class="imgtext">
+            <div class="userimg">
+              <img src="img interview/arnaud.jpg" class="cover" />
+            </div>
+            <h4>Arnaud Dumoncel <br /><span class="online">online</span></h4>
+          </div>
+          <ul class="nav_icons">
+            <li class="cam"><ion-icon name="videocam-outline"></ion-icon></li>
+            <li class="call"><ion-icon name="call-outline"></ion-icon></li>
+            <li><ion-icon name="search-outline"></ion-icon></li>
+            <li><ion-icon name="ellipsis-vertical"></ion-icon></li>
+          </ul>
+        </div>
+        <!--chatbox-->
+        <div class="chatbox">
+          <div class="message my_message">
+            <p>
+              Bonsoir Arnaud merci d'avoir accepté mon interview pour ma
+              préformation
+              <br />
+              je vais vous poser quelques questions cela prendra que quelques
+              minutes merci à vous.
+              <span>20:15</span>
+            </p>
+          </div>
+          <div class="message frnd_message">
+            <p>
+              Bonsoir Sebastien, avec plaisir je réponderai à tes questions
+              allons-y
+              <br /><span>12:15</span>
+            </p>
+          </div>
+          <div class="message my_message">
+            <p>
+              Alors pourrais-tu te présenter ? Peux-tu nous parler de ton
+              parcours avant de devenir développeur web et de ce qui t'a amené à
+              ce métier ?<span>20:16</span>
+            </p>
+          </div>
+          <div class="message frnd_message">
+            <p>
+              Je m'appelle
+              <a target="_blank" href="image video/arnaud.png"
+                >Arnaud Dumoncel</a
+              >, je suis Développeur Javascript chez
+              <a
+                target="_blank"
+                href="https://www.dastra.eu/fr?gad=1&gclid=CjwKCAjwv-2pBhB-EiwAtsQZFKeQCTXPyz0p8HrC8NjhVRsIfnWSjYBEDr4QMbDRmHgIxa0IKmsLZhoCFskQAvD_BwE"
+                >Dastra</a
+              >, une startup proposant une application qui aide les DPO (Data
+              Protection Officer) des entreprises à être conformes au RGPD.Avant
+              d'être développeur,<br />j'ai fait un Bac +3 en finance et j'ai
+              travaillé dans le commerce et dans la banque.J'ai jamais réussi à
+              me passionner dans ce que je faisais à ce moment là. À un moment
+              donné j'en ai eu marre de ma pas me sentir à l'aise dans ma vie
+              pro et j'ai fait un bilan de compétences. À la fin du bilan, on
+              m'a proposé plusieurs jobs, dont le développement. Je me suis dit
+              que j'allais me renseigner, j'ai discuté avec d'autres
+              développeurs, des chefs de projets, des responsables d'agence web
+              etc. J'ai commencé à apprendre tout seul et je me suis vite pris
+              au jeu. Après ça, j'ai décidé de suivre une formation pour en
+              faire mon métier. J'ai choisi la formation développeur web
+              d'Openclassrooms.
+              <span>20:17</span>
+            </p>
+          </div>
+          <div class="message my_message">
+            <p>
+              2e question : Quelles sont vos qualifications?<span>20:17</span>
+            </p>
+          </div>
+          <div class="message frnd_message">
+            <p>
+              J'ai un certificat de développeur web obtenu avec Openclassrooms.
+              Je me suis spécialisé en Javascript et principalement VueJS.<br /><span
+                >20:18</span
+              >
+            </p>
+          </div>
+          <div class="message my_message">
+            <p>
+              3e question : pourriez-vous nous décrire votre parcours
+              professionnel jusqu'à présent ?<br /><span>20:18</span>
+            </p>
+          </div>
+          <div class="message frnd_message">
+            <p>
+              J'ai travaillé principalement entant que conseiller financier et à
+              différents postes dans le commerce. Depuis ma formation je suis
+              développeur web.<br /><span>20:19</span>
+            </p>
+          </div>
+          <div class="message my_message">
+            <p>
+              4e question : comment se déroule une journée type pour un
+              développeur web ?<br /><span>20:19</span>
+            </p>
+          </div>
+          <div class="message frnd_message">
+            <p>
+              Comment se déroule une journée type pour un développeur web ? Ça
+              va dépendre de l'employeur, j'ai été développeur dans 3
+              entreprises et aucune n'avait la même organisation. Aujourd'hui
+              chez Dastra toute l'équipe technique est 100% en télétravail. On
+              travaille en sprints de 2 semaines dans lesquels on remplit des
+              "user stories" (des tâches). Chaque lundi en début de sprint on
+              fait un sprint planning toute la matinée, durant lequel on
+              priorise les tâches à faire pendant le sprint à l'aide du backlog
+              (c'est notre liste de user stories). Chaque vendredi de fin de
+              sprint on fait la sprint review pendant l'après-midi, ça permet
+              aux développeurs de présenter au Product Owner les tâches
+              réalisées pendant le sprint et de s'assurer qu'elles soient
+              conformes aux besoins. On a un daily chaque matin à 9h où on fait
+              un debrief de la veille et on pose des questions (si on en a) sur
+              les tâches qu'on a dans la journée. Ça dure entre 15 et 30
+              minutes. Ensuite on se met sur nos tâches, parfois on a des
+              réunions dans la journée avec les autres développeurs sur les
+              sujets en commun. En ce moment on essaye de faire du pair
+              programming une fois par semaine. <br /><span>20:20</span>
+            </p>
+          </div>
+          <div class="message my_message">
+            <p>
+              Question numéro 5: quel est le projet le plus important que vous
+              ayez réalisé à ce jour en tant que développeur ?<br /><span
+                >20:21</span
+              >
+            </p>
+          </div>
+          <div class="message frnd_message">
+            <p>
+              C'est surement celui sur lequel je suis actuellement. Je travaille
+              sur la refonte technique du site. On change complètement de
+              technologie en frontend et en backend, ça prend pas mal de temps
+              et ça demande beaucoup d'ajustements pour avoir quelque chose qui
+              fonctionne. On en profite aussi pour faire la refonte graphique du
+              site.<br /><span>20:21</span>
+            </p>
+          </div>
+          <div class="message my_message">
+            <p>
+              6e question: où vous voyez-vous dans 5 à 8 ans en tant que
+              développeur ?<br /><span>20:22</span>
+            </p>
+          </div>
+          <div class="message frnd_message">
+            <p>
+              Honnêtement je ne sais pas. J'aime beaucoup ce que je fais
+              aujourd'hui. J'aime bien apprendre des nouvelles technos, outils,
+              méthodes etc. Je ne sais pas où ça va me mener et ça ne m'inquiète
+              pas trop, je sais qu'il y a un paquet de sujets qui me branchent
+              dans ce milieu. <br /><span>20:23</span>
+            </p>
+          </div>
+          <div class="message my_message">
+            <p>
+              7e et dernière question : Pourrais-tu me donner un conseil pour
+              devenir développeur web et les choses essentielles à connaître
+              ?<span>20:23</span>
+            </p>
+          </div>
+          <div class="message frnd_message">
+            <p>
+              Ça fait un peu bateau de dire ça, mais il faut être motivé et
+              patient. Il y a beaucoup de monde qui se forme au métier de
+              développeur. C'est un métier très recherché sur le marché de
+              l'emploi, mais les entreprises cherchent beaucoup de confirmés /
+              seniors, et il y a un grand nombre de juniors. Il ne faut pas se
+              contenter de ce qu'on apprend pendant les formations, il faut
+              avoir des projets à montrer pour se démarquer et ne pas avoir peur
+              d'être frustré quand on ne comprend pas un concept ou que le code
+              ne fonctionne pas du premier coup. <br /><span>20:24</span>
+            </p>
+          </div>
+          <div class="message my_message">
+            <p>
+              Merci pour les conseils, et encore un grand merci de m'avoir
+              accordé cette interview pour ma préformation. Je vous souhaite le
+              meilleur dans votre carrière de développeur web, et je vous
+              souhaite une agréable soirée.<br /><span>20:24</span>
+            </p>
+          </div>
+          <div class="message frnd_message">
+            <p>
+              De rien, je vous souhaite également de réussir en tant que
+              développeur web ainsi que dans votre préformation ! Je vous
+              souhaite également une bonne soirée.<br /><span>20:25</span>
+            </p>
+          </div>
+          <div class="message frnd_message">
+            <p>Courage pour ta préformation. <br /><span>09:00</span></p>
+          </div>
+        </div>
+        <div class="chatbox_input">
+          <ion-icon name="happy-outline"></ion-icon>
+          <ion-icon name="attach-outline"></ion-icon>
+          <input type="text" placeholder="Taper un message " />
+          <ion-icon name="send-outline"></ion-icon>
+          <ion-icon name="mic"></ion-icon>
+        </div>
+      </div>
+    </div>
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+    ></script>
+  </body>
+</html>
