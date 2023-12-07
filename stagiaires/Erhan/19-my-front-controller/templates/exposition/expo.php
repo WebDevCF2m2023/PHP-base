69916a1423e81316f26b366b5925cab0f57aff7c
erdaserhan <erhan209@gmail.com>
2023-12-07 16:20:45 +0100

    Ex19

---
+++ stagiaires/Erhan/19-my-front-controller/templates/exposition/expo.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Exposition</title>
+</head>
+<body class="foto-expo">  
+  <div class="navbar">
+    <div class="logo">
+      <h4><a href="index.php">Erhan ERDAS</a></h4>
+    </div>
+    <nav class="navbar-sections">
+      <a href="./">Accueil</a>
+      <a href="./?page=interview">Interview</a>
+      <a href="./?page=rapport">Conclusion</a>
+      <a href="expo">Exposition</a>
+      <a href="./?page=galerie">Photos</a>
+    </nav>
+  </div>
+  <div class="main">
+    <div class="francais1">
+      <h1><a id="nam" href="https://www.nam-ip.be/en/front-page-english/" target="_blank">NAM - IP</a> Computer Museum</h1>
+      <br>
+      <p>
+        Je suis surpris de découvrir qu'il existe un musée de l'informatique à Namur. Namur est la première ville que j'ai visitée plusieurs fois en Belgique. Mais je n'avais aucune idée de ce musée. Le début de la visite a été vraiment accueillant avec quelques jeux qui nous ont permis de nous faire une idée de la pensée informatique. Lorsque nous avons commencé la visite, la première chose que j'ai vue était l'Atari qui me rappelait mon enfance. C'était le premier jouet auquel je jouais avec mes frères et mes amis. Je me souviens encore du nombre de fois où j'ai demandé à ma mère de m'offrir un Atari. La fois où elle m'a surpris avec ce jouet, j'étais sur le point de pleurer et j'étais tellement excité.  
+        <br><br>Une autre chose intéressante était les dactylos. Cela fait des années que je n'en ai pas vu un seul. En fait, mon père travaillait pour le tribunal et chaque fois que je lui rendais visite, j'essayais d'écrire quelque chose avec le dactylo. Je n’avais pas d’ordinateur à cette époque et c’était le seul outil que je pouvais remplacer. C'était vraiment amusant.
+        <br><br>Quelques années plus tard, mon père et ses collègues ont été obligés de remplacer le dactylo par des ordinateurs. Je me souviens encore que c'était tellement compliqué pour eux de s'habituer au nouveau système. Ils ne savaient pas comment l'utiliser. Mon père nous demandait de l’aide mais nous n’en avions aucune idée non plus. C'était l'occasion pour nous de lui demander de nous acheter un ordinateur. Nous pourrions donc lui apprendre à s'en servir.  
+        <br><br>C'est ainsi que j'ai eu mon premier ordinateur à la maison. J'étais vraiment heureux et excité avec mes frères d'avoir un ordinateur à la maison. Mais le problème était que nous ne savions pas non plus comment l’utiliser. Nous avons découvert le principe de son utilisation au quotidien. Nous jouions aux jeux de base et essayions de comprendre comment cela fonctionne réellement. Après des années, j'ai essayé de changer les pièces à l'intérieur de l'ordinateur. C'est ainsi que j'ai pu découvrir le squelette de l'ordinateur.
+      </p>
+    </div>  
+  </div>
+</body>
+</html>
---
+++ stagiaires/Erhan/19-my-front-controller/templates/exposition/galerie.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css"><title>Galerie</title>
+</head>
+<body class="foto-galerie">
+  <div class="navbar">
+    <div class="logo">
+      <h4><a href="index.php">Erhan ERDAS</a></h4>
+    </div>
+    <nav class="navbar-sections">
+      <a href="?page=accueil">Accueil</a>
+      <a href="?page=interview">Interview</a>
+      <a href="?page=rapport">Conclusion</a>
+      <a href="?page=expo">Exposition</a>
+      <a href="?galerie">Photos</a>
+    </nav>
+  </div>
+  <div class="full-img" id="fullImgBox">
+        <img src="image/eye.jpg" id="fullImg">  
+        <span onclick="closefullImg()">X</span>
+  </div>
+  <div class="img-galerie">
+        <img src="image/musee1.JPG" onclick="openfullImg(this.src)">
+        <img src="image/musee2.JPG" onclick="openfullImg(this.src)">
+        <img src="image/musee3.JPG" onclick="openfullImg(this.src)">
+        <img src="image/musee4.JPG" onclick="openfullImg(this.src)">
+        <img src="image/musee5.JPG" onclick="openfullImg(this.src)">
+        <img src="image/musee6.JPG" onclick="openfullImg(this.src)"> 
+        <img src="image/musee7.JPG" onclick="openfullImg(this.src)"> 
+        <img src="image/musee8.JPG" onclick="openfullImg(this.src)"> 
+        <img src="image/musee9.JPG" onclick="openfullImg(this.src)"> 
+        <img src="image/musee10.JPG" onclick="openfullImg(this.src)"> 
+        <img src="image/musee11.JPG" onclick="openfullImg(this.src)"> 
+        <img src="image/musee12.JPG" onclick="openfullImg(this.src)"> 
+  </div>
+  <script>
+        var fullImgBox = document.getElementById('fullImgBox'); 
+        var fullImg = document.getElementById('fullImg'); 
+
+        function openfullImg(pic){
+            fullImgBox.style.display = 'flex';
+            fullImg.src = pic;
+        }
+
+        function closefullImg(){
+            fullImgBox.style.display = 'none';
+        }
+      </script>  
+    </body>
+</html>
