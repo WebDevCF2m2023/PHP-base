d43ca36dba0f04e781e5a10b6c98784a8b637d95
sebastien2506 <jestrovic_8@msn.com>
2023-12-08 10:42:55 +0100

    404

--- stagiaires/Sebastien/19.my-front-controller/template/404.php
+++ stagiaires/Sebastien/19.my-front-controller/template/404.php
@@ -7,15 +7,31 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
     <title><?=$title?></title>
-    <link href="css/style.css" rel="stylesheet">
+    <style>
+
+        @font-face {
+            font-family: "byl";
+            src: url("fond/byl.otf");
+        }
+
+        body{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background:url("image video/404.webp");
+        }
+
+        h1{
+            color:white;
+            text-align:center;
+            font-size: 8rem;
+            margin-top: 15vh;
+            font-family: "byl", sans-serif;
+        }
+    </style>
 </head>
 <body>
-    <header>
-    <h1>error 404</h1>
-    </header>
-    <section>
-        <h2><?=$title?></h2>
-        <p>La page demandée n'existe pas.</p>
-    </section>>
+    <h1>PAGE NOT FOUND</h1>
 </body>
 </html>
