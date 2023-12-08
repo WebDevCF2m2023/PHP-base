169c2c802345e12717f69f7b58c955fd2cc00159
Tevin01 <tevin.eliya.bofona@cf2000.onmicrosoft.com>
2023-12-08 12:08:47 +0100

    Finally, 19 is here!

---
+++ stagiaires/Tevin/site-Tevin/templates/Musee.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visite au Computer Museum NAM-IP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <ul>
+        <li>
+            <a href="?section=index">Go back</a>
+        </li>
+        </ul>
+    </nav>
+    <h1>Can't see forward if the past is unknown</h1>
+    <p>C'est grâce au passé qu'il possible d'aller vers l'avenir ; autrement, il est quasiment impossible de choisir la bonne course des choses et améliorer tout en gardant le « noyau ».</p>
+    <p>Ceci s'applique d'autant plus quand on parle de technologie, comme ici, l'informatique et ces anciens ordinateurs. </p>
+    <section id="images">
+        <img src="Image/20231005_133432.jpg" width="400px">
+        <br>
+        <img src="Image/20231005_133233.jpg" width="200px">
+        <img src="Image/20231005_133220.jpg" width="200px">
+    </section>
+    <h2>Microsoft</h2>
+    <p>L'un des grands mastodontes de notre ère actuelle.</p>
+    <p>Il est également derrière la construction des premiers ordinateurs avec son propre système d'exploitation inclus, sans compter que même encore aujourd'hui, Microsoft aspire toujours aux progrès.</p>
+    <section id="images">
+        <img src="Image/20231005_134836.jpg" width="200px">
+        <img src="Image/20231005_134822.jpg" width="200px">
+        <img src="Image/20231005_134829.jpg" width="200px">
+        <img src="Image/20231005_135353.jpg" width="200px">
+    </section>
+    <h2>People's <span class="Requirement">Need</span> and <span class="Sins">Greed</span></h2>
+    <P>L'être humain à des besoins pour subvenir à son bonheur ou aux taches du quotidien.</P>
+    <P>Eventuellement, une fois ses besoins comblés et s'il en a la possibilité, il y a de grande chance qu'il finira par chercher un moyen plus efficace que le précédent pour améliorer son train de vie.</P>
+    <P>Et c'est là précisément que l'évolution fait son entrée !</P>
+    <p>C'est grâce aux besoins du peuple et leur avarice que notre monde est en évolution constante… et ce, pour le meilleur comme le pire.</p>
+</body>
+</html>
