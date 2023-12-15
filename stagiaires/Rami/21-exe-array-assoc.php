2343f81e1cdf9c5cb75102d3c8dece897746d6b8
rammroumm2K <ramybrz00@gmail.com>
2023-12-15 13:17:34 +0100

    holaaa

---
+++ stagiaires/Rami/21-exe-array-assoc.php
@@ -0,0 +1,13 @@
+<?php
+$monTabAssoc =  [
+"prenom" => "Rami",
+"nom" => "Ben Salem",
+"caracteristique" => "rien",
+"dateDeNaissance" => "02-11-2000",];
+
+echo $monTabAssoc["prenom"]. '<hr>';
+echo $monTabAssoc["nom"]. '<hr>';
+echo $monTabAssoc["caracteristique"]. '<hr>';
+echo $monTabAssoc["dateDeNaissance"]. '<hr>';
+
+exit();
