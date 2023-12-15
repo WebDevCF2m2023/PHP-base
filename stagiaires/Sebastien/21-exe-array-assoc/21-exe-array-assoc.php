33eaf6e81a850bbba1d7b533bc0a13ff57cd4c37
sebastien2506 <jestrovic_8@msn.com>
2023-12-15 11:27:23 +0100

    1

---
+++ stagiaires/Sebastien/21-exe-array-assoc/21-exe-array-assoc.php
@@ -0,0 +1,14 @@
+<?php
+
+$monTabAssoc =  [
+    "prenom" => "Sebastien",
+    "nom" => "Struys",
+    "caracteristique" => "sympas",
+    "dateDeNaissance" => "1986-06-25",
+];
+
+
+echo $monTabAssoc ["prenom"].'<hr>';
+echo $monTabAssoc ["nom"].'<hr>';
+echo $monTabAssoc ["caracteristique"].'<hr>';
+echo $monTabAssoc ["dateDeNaissance"].'<hr>';
