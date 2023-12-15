5bd357668036753445cb778a1323aad5e7c7dcd8
armaxro <arnaldomrojas@outlook.com>
2023-12-15 09:54:15 +0100

    20-exe array

---
+++ stagiaires/Maxi/20-exe-array-index.php
@@ -0,0 +1,23 @@
+<?php
+
+$monTab = ['un', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf', 'dix'];
+
+echo $monTab[0];
+echo '<hr>';
+echo $monTab[1];
+echo '<hr>';
+echo $monTab[3];
+echo '<hr>';
+echo $monTab[4];
+echo '<hr>';
+echo $monTab[5];
+echo '<hr>';
+echo $monTab[6];
+echo '<hr>';
+echo $monTab[7];
+echo '<hr>';
+echo $monTab[8];
+echo '<hr>';
+echo $monTab[9];
+echo '<hr>';
+
