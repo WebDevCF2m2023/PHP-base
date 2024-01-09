71d5a296a1ee347fe0b188ccf881bf63cbba0bb5
mikhawa <michaeljpitz@gmail.com>
2024-01-09 16:01:05 +0100

    rm2

--- 25-boucle-for.php
+++ 25-boucle-for.php
@@ -7,9 +7,7 @@
  * 1) Affichez les nombres de 10 à 1 avec une boucle for
  */
 echo "<p>1) Affichez les nombres de 10 à 1 avec une boucle for<br>";
-for($i=10;$i>0;$i--){
-    echo $i.' ';
-}
+
 echo "</p>";
 
 /*
@@ -17,9 +15,7 @@ echo "</p>";
  */
 
 echo "<p>2) Affichez les nombres pairs de 0 à 20 avec une boucle for<br>";
-for($i=0;$i<=20;$i+=2){
-    echo $i.' ';
-}
+
 echo "</p>";
 
 /*
@@ -28,10 +24,7 @@ echo "</p>";
 
 echo "<p>3) Affichez les nombres de 1 à 20 avec une boucle for en calculant la somme de ces nombres<br>";
 $somme=0;
-for($i=1;$i<=20;$i++){
-    echo $i.' ';
-    $somme+=$i;
-}
+
 echo " - La somme des nombres de 1 à 20 est : ".$somme;
 echo "</p>";
 
@@ -42,15 +35,7 @@ echo "</p>";
 
 echo "<p>4) Affichez les nombres de 1 à 20 avec une boucle for en calculant la somme de ces nombres et en affichant à chaque tour si le nombre est pair ou impair<br>";
 $somme=0;
-for($i=1;$i<=20;$i++){
-    echo $i.' ';
-    $somme+=$i;
-    if($i%2==0){
-        echo " - pair | ";
-    }else{
-        echo " - impair | ";
-    }
-}
+
 echo " - La somme des nombres de 1 à 20 est : ".$somme;
 echo "</p>";
 
@@ -61,16 +46,7 @@ echo "</p>";
 
 echo "<p>5) Affichez la somme des nombres de 1 à 20 à chaque tour avec une boucle for et en affichant à chaque tour si la somme est pair ou impair<br>";
 $somme=0;
-for($i=1;$i<=20;$i++){
 
-    $somme+=$i;
-    echo " - La somme est : ".$somme;
-    if($somme%2==0){
-        echo " - pair | ";
-    }else{
-        echo " - impair | ";
-    }
-}
 echo "</p>";
 
 /*
@@ -85,10 +61,5 @@ echo "</p>";
  */
 
 echo "<p>6) Utilisez deux boucles for imbriquées pour afficher une table de multiplication de 1 à 5.<br>";
-for($i=1;$i<=5;$i++){
-    for($j=1;$j<=5;$j++){
-        echo $i.' x '.$j.' = '.($i*$j).' | ';
-    }
-    echo "<br>";
-}
+
 echo "</p>";
