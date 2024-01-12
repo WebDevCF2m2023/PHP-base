7d3cf47b614924f1bba0d5459e16da442c2aad92
erdaserhan <erhan209@gmail.com>
2024-01-12 10:04:12 +0100

    23-24

--- stagiaires/Erhan/23-operateurs-logiques.php
+++ stagiaires/Erhan/23-operateurs-logiques.php
@@ -17,9 +17,15 @@ if($page < 10 || $page > 20){
     echo "2) La page $page est entre 10 et 20<br>";
 }
 
+if($page < 10 || $page > 20):
+    echo "2) La page $page est soit inférieure à 10, soit supérieure à 20<br>";
+else:
+    echo "2) La page $page est entre 10 et 20<br>";
+endif;
+
 $note = mt_rand(5, 15);
 
-if($note < 10 xor $note > 10){
+if($note <= 10 xor $note >= 10){
     echo "3) La $note est soit inférieure à 10, soit supérieure à 10<br>";
 }else {
     echo "3) La $note est 10<br>";
