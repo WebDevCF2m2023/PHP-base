2f637707d2714d3d7f9a5e323bea6ddec46e7910
WebDevCF2m <gitweb@cf2m.be>
2024-01-17 11:12:04 +0100

    25 corrected for

--- formateur/25-boucle-for.php
+++ formateur/25-boucle-for.php
@@ -10,13 +10,20 @@
 
 echo "<p>1) Affichez les nombres de 10 à 1 avec une boucle for<br>";
 
+for($i=10; $i>=1; $i--){
+   echo "$i ";
+}
+
 echo "</p>";
 
 /*
  * 2) Affichez les nombres pairs de 0 à 20 avec une boucle for
  */
 
-echo "<p>2) Affichez les nombres pairs de 0 à 20 avec une boucle for<br>";
+echo "<p>2) Affichez les nombres pairs de 0 à 20 avec une boucle for<br> 
+Ne pas utiliser les { et } ne fonctionne que sur une ligne (qui finit au ;), le ||| n'est affiché qu'après la boucle for<br>";
+
+for($i=0; $i<=20; $i+=2) echo "$i "; echo "<br>|||";
 
 echo "</p>";
 
@@ -26,6 +33,11 @@ echo "</p>";
 
 echo "<p>3) Affichez les nombres de 1 à 20 avec une boucle for en calculant la somme de ces nombres<br>";
 $somme=0;
+
+for($i=1; $i<=20; $i++):
+   echo "$i ";
+   $somme += $i; // $somme = $somme + $i
+endfor;
 
 echo " - La somme des nombres de 1 à 20 est : ".$somme;
 echo "</p>";
@@ -38,6 +50,15 @@ echo "</p>";
 echo "<p>4) Affichez les nombres de 1 à 20 avec une boucle for en calculant la somme de ces nombres et en affichant à chaque tour si le nombre est pair ou impair<br>";
 $somme=0;
 
+for($i=1; $i<=20; $i++):
+   $somme += $i;
+   if($i%2 === 0):
+      echo "$i : pair | ";
+   else:
+      echo "$i : impair | ";
+   endif;
+endfor;
+
 echo " - La somme des nombres de 1 à 20 est : ".$somme;
 echo "</p>";
 
@@ -48,6 +69,17 @@ echo "</p>";
 
 echo "<p>5) Affichez la somme des nombres de 1 à 20 à chaque tour avec une boucle for et en affichant à chaque tour si la somme est pair ou impair<br>";
 $somme=0;
+
+for($i=1;$i<=20;$i++){
+   $somme += $i;
+   echo " - La somme est : $somme ";
+   // si le modulo vaut 0 (false), on l'inverse => true
+   if(!($somme%2)){
+      echo " - pair | ";
+   }else{
+      echo " - impair | ";
+   }
+}
 
 echo "</p>";
 
@@ -63,5 +95,12 @@ echo "</p>";
  */
 
 echo "<p>6) Utilisez deux boucles for imbriquées pour afficher une table de multiplication de 1 à 5.<br>";
+
+for($i=1;$i<=5;$i++){
+   for($j=1;$j<=5;$j++){
+      echo " $i x $j = ".($i*$j)." | ";
+   }
+   echo "<br>";
+}
 
 echo "</p>";
