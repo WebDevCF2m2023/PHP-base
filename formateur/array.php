facfbb4186d5ade309a2a1166383fcb607baf4dc
mikhawa <michaeljpitz@gmail.com>
2024-01-22 10:09:42 +0100

    dans array.php

--- formateur/array.php
+++ formateur/array.php
@@ -11,6 +11,9 @@ $classe=['Jean','Eric','Marie','Julie','Luc','Pierre','Paul','Jacques','Hélène
 // jours de la semaine en français
 $semaineFr=[1 =>'lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche',];
 
+// mois de l'année en français
+$moisFr=[1 =>'janvier','février','mars','avril','mai','juin','juillet','août','septembre','octobre','novembre','décembre',];
+
 // Tableau de données représentant une classe de 18 élèves
 $classe18 = [
     ['nom' => 'Élève 1', 'age' => 15, 'note' => 90],
